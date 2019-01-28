4fbf5eddde00134a67bab880fc5d6af406876a39
strikerJava <rtkanall@gmail.com>
2019-01-27 23:23:15 -0500

    Webserver running; database creator added

---
+++ Project2/database_setup.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Inventory(Base):
+    __tablename__ = 'inventory'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(250), nullable = False)
+    price = Column(Float)
+    description = Column(String(500))
+
+engine = create_engine('sqlite:///inventory.db')
+
+Base.metadata.create_all(engine)
--- Project2/main.py
+++ Project2/main.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base
 app = Flask(__name__)
+#Base = declarative_Base()
+engine = create_engine('sqlite:///inventory.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 @app.route('/')
 
