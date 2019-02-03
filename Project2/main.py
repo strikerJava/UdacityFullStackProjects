c74420a5bd80d2cb7a631348279ad45075c6cbaa
strikerJava <rtkanall@gmail.com>
2019-02-02 21:13:49 -0500

    added aditional routes for DB app

--- Project2/main.py
+++ Project2/main.py
@@ -2,8 +2,11 @@ from flask import Flask
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base
+
+from flask import session as loginState #login sesson object
+import random, string
+
 app = Flask(__name__)
-#Base = declarative_Base()
 engine = create_engine('sqlite:///inventory.db')
 Base.metadata.bind = engine
 
@@ -11,10 +14,28 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 @app.route('/')
-
 def startPoint():
     return "Hello Flask"
 
+@app.route('/oAuth')
+def oAuthLogIn():
+	sessionToken = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+	loginState['state'] = sessionToken
+	return "Token: %s" %loginState['state']
+
+@app.route('/logOut')
+def logout():
+    return "logout page here"
+
+@app.route('/database', methods = ['GET', 'POST'])
+def getAllDataBase():
+    return "Database listing here"
+
+@app.route('/test')
+def reset():
+    return "Reset Login State. Please return to main page"
+
 if __name__ == '__main__':
+    app.secret_key = 'dummyKey'
     app.debug = True
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host = '0.0.0.0', port = 8080)
