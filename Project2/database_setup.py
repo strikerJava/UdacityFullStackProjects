6bea5bd01ce5ba4428213f46986caaee65b5ebff
strikerJava <rtkanall@gmail.com>
2019-02-24 15:30:40 -0500

    ADded last few days progress

--- Project2/database_setup.py
+++ Project2/database_setup.py
@@ -3,9 +3,18 @@ import sys
 from sqlalchemy import Column, ForeignKey, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine,  UniqueConstraint
 
 Base = declarative_base()
+
+
+class Category(Base):
+    __tablename__ = 'itemCategory'
+
+    id = Column(Integer, primary_key = True)
+    categoryName = Column(String(250), nullable = False)
+    categoryDescript = Column(String(250))
+    UniqueConstraint('categoryName')
 
 class Inventory(Base):
     __tablename__ = 'inventory'
@@ -14,6 +23,8 @@ class Inventory(Base):
     name = Column(String(250), nullable = False)
     price = Column(Float)
     description = Column(String(500))
+    quantity = Column(Integer)
+    categoryID = Column(Integer, ForeignKey(Category.id), nullable=False)
 
 
 engine = create_engine('sqlite:///inventory.db')
--- Project2/main.py
+++ Project2/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Inventory
+from database_setup import Base, Inventory, Category
 
 from flask import session as loginState #login sesson object
 
@@ -15,7 +15,7 @@ session = DBSession()
 
 @app.route('/')
 def startPoint():
-    return render_template('loginPage.html')
+    return render_template('mainPage.html')
 
 @app.route('/oAuth')
 def oAuthLogIn():
@@ -24,6 +24,10 @@ def oAuthLogIn():
     return render_template('loginPage.html', STATE=sessionToken)
     #return "Token: %s" %loginState['state'
 
+@app.route('/test')
+def reset():
+    loginState['state'] = 0
+    return "Reset Login State. Please return to main page; token state: %s" %loginState['state']
 
 @app.route('/logOut')
 def logout():
@@ -51,34 +55,71 @@ def deleteSingleItem(idInt):
 @app.route('/state')
 def status():
     return "State: Token: %s" %loginState['state'] 
-	
-@app.route('/test')
-def reset():
-    loginState['state'] = 0
-    return "Reset Login State. Please return to main page; token state: %s" %loginState['state']
+
 
 def getAllItems():
     #allitem = session.query(Base).all()
 	print "Get all items"
     #print allitem
 
+
 @app.route('/addItemForm')
 def addItemForm():
     return render_template('newItem.html')
 
+
+@app.route('/addNewCategory')
+def addNewCategoryForm():
+    return render_template('newCategory.html')
+
+
 @app.route('/makeItem', methods = ['POST'])
 def makeNewItem():
     if request.method == 'POST':
-        newItem = Inventory(name = request.form['name'], price = .99, description = request.form['description'])
+        newItem = Inventory(name = request.form['name'], price = request.form['price'], description = request.form['description'], categoryID = 0, quantity = request.form['quantity'])
     session.add(newItem)
     session.commit()
-    return "OK"
+    return "Added new item"
 
-def updateItem():
-    print "Update an item"
-	
+
+@app.route('/makeNewCategory', methods = ['POST'])
+def makeNewCategory():
+    if request.method == 'POST':
+        newCategory = Category(categoryName = request.form['name'], categoryDescript = request.form['categoryDescript'])
+    session.add(newCategory)
+    session.commit()
+    return "new Category Added."
+
+@app.route('/deleteItem/<int:idInt>')
+def deleteItemByID(idInt):
+    itemToDelete = session.query(Inventory).get(idInt)
+
+
+    return "garbage"
+@app.route('/itemInfo/<int:idInt>')
+def getItemFullInfo(idInt):
+    return "empty"
+
+@app.route('/updateItem/<int:idInt>')
+def updateItem(idInt):
+    updateItem = session.query(Inventory).get(idInt)
+    return render_template('updatePage.html', updateItem=updateItem)
+
+@app.route('/itemUpdated', methods = ['POST'])
+def commitChanges():
+    updateThisItem = session.query(Inventory).get(request.form['ID'])
+    updateThisItem.name = request.form['name']
+    updateThisItem.price = request.form['price']
+    updateThisItem.description = request.form['description']
+    updateThisItem.quantity = request.form['quantity']
+    session.add(updateThisItem)
+    session.commit()
+    return "Updated Item"
+
+
 def deleteSingleItem():
     print "delete a single Item"
+
 
 def getItemByID():
     print "Get item"
